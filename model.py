0c083d69d1f179580e1c0ff391b79e5d8ad026b2
toth bandi <tot70and@gmail.com>
2017-12-14 16:52:08 +0100

    todo - refactor, done with classes

--- model.py
+++ model.py
@@ -1,85 +1,86 @@
-def print_usage():
-    print('''Command Line Todo application
-=============================
+class Model(object):
 
-Command line arguments:
- -l   Lists all the tasks
- -a   Adds a new task
- -r   Removes an task
- -c   Completes an task''')
+    def __init__(self):
+        self.file_name = 'todo_list.txt'
 
-def print_todo_list():
-    file_name = 'todo_list.txt'
-    try:
-        my_file = open(file_name, 'r')
-        lines = my_file.readlines()
-        if len(lines) == 0:
-            print('No todos for today! :)')
+    def print_usage(self):
+        print('''Command Line Todo application
+    =============================
+
+    Command line arguments:
+    -l   Lists all the tasks
+    -a   Adds a new task
+    -r   Removes an task
+    -c   Completes an task''')
+
+    def print_todo_list(self):
+        try:
+            my_file = open(self.file_name, 'r')
+            lines = my_file.readlines()
+            if len(lines) == 0:
+                print('No todos for today! :)')
+            else:
+                for i in range(len(lines)):
+                    print('{} - {}'.format(i + 1, lines[i]), end='')
+                print()
+            my_file.close()
+        except IOError:
+            print('cannot open', self.file_name)
+
+    def add_todo(self, task):
+        try:
+            my_file = open(self.file_name, 'a')
+            my_file.write('\n[ ] ' + task )
+            my_file.close()
+        except IOError:
+            print('cannot open', self.file_name)
+
+    def lines_from_file(self):
+        try:
+            my_file = open(self.file_name, 'r')
+            lines = my_file.readlines()
+            my_file.close()
+        except IOError:
+            print("cannot open", self.file_name)
+        return lines
+
+    def lines_to_file(self, lines):
+        try:
+            my_file = open(self.file_name, 'w')
+            for line in lines:
+                my_file.write(line)
+            my_file.close()
+        except IOError:
+            print("cannot open", self.file_name)
+        return lines
+
+    def check_todo(self, check):
+        check_number = int(check)
+        lines = self.lines_from_file()
+        if check_number <= len(lines) and check_number > 0:
+            line = lines[check_number - 1].split(' ')
+            if line[0].upper() != '[X]':
+                line.pop(0)
+                line[0] = '[X]'
+            else:
+                line[0] = '[ ]'
+            lines[check_number - 1] = ' '.join(line)
+            self.lines_to_file(lines)        
         else:
-            for i in range(len(lines)):
-                print('{} - {}'.format(i + 1, lines[i]), end='')
-            print()
-        my_file.close()
-    except IOError:
-        print("cannot open", file_name)
+            print('Unable to check: index is out of bound')
+            self.print_usage()    
 
-def add_todo(task):
-    file_name = 'todo_list.txt'
-    try:
-        my_file = open(file_name, 'a')
-        my_file.write('\n[ ] ' + task )
-        my_file.close()
-    except IOError:
-        print("cannot open", file_name)
-
-def lines_from_file(file_name):
-    try:
-        my_file = open(file_name, 'r')
-        lines = my_file.readlines()
-        my_file.close()
-    except IOError:
-        print("cannot open", file_name)
-    return lines
-
-def lines_to_file(file_name, lines):
-    try:
-        my_file = open(file_name, 'w')
-        for line in lines:
-            my_file.write(line)
-        my_file.close()
-    except IOError:
-        print("cannot open", file_name)
-    return lines
-
-def check_todo(check):
-    check_number = int(check)
-    file_name = 'todo_list.txt'
-    lines = lines_from_file(file_name)
-    if check_number <= len(lines) and check_number > 0:
-        line = lines[check_number - 1].split(' ')
-        if line[0].upper() != '[X]':
-            line.pop(0)
-            line[0] = '[X]'
+    def remove_todo(self, number):
+        number = int(number)
+        lines = self.lines_from_file()
+        if number <= len(lines) and number > 0:
+            lines.pop(number - 1)
+            if lines[-1][-1] == '\n':
+                lines[-1] = lines[-1][:-1]
+            self.lines_to_file(lines)       
         else:
-            line[0] = '[ ]'
-        lines[check_number - 1] = ' '.join(line)
-        lines_to_file(file_name, lines)        
-    else:
-        print('Unable to check: index is out of bound')
-        print_usage()    
-
-def remove_todo(number):
-    number = int(number)
-    file_name = 'todo_list.txt'
-    lines = lines_from_file(file_name)
-    if number <= len(lines) and number > 0:
-        lines.pop(number - 1)
-        if lines[-1][-1] == '\n':
-            lines[-1] = lines[-1][:-1]
-        lines_to_file(file_name, lines)       
-    else:
-        print('Unable to remove: index is out of bound')
-        print_usage()
+            print('Unable to remove: index is out of bound')
+            self.print_usage()
  
 
         
--- todo.py
+++ todo.py
@@ -1,46 +1,52 @@
 import sys
 import model
 
-def arguments_validator():
-    if len(sys.argv) == 1:
-        return True
-    elif len(sys.argv) == 2:
-        if sys.argv[1] == '-l':
-            return True
-        elif sys.argv[1] == '-a':
-            print('Unable to add: no task provided')
-        elif sys.argv[1] == '-c':
-            print('Unable to check: no index provided')
-        elif sys.argv[1] == '-r':
-            print('Unable to remove: no index provided')                    
-        else:
-            print('Unsupported argument\n')
-    elif len(sys.argv) == 3:
-        if sys.argv[1] == '-a':
-            return True
-        elif sys.argv[1] == '-c' or sys.argv[1] == '-r':
-            if sys.argv[2].isdigit():
-                return True
-            else:
-                print('Unable to remove: index is not a number')
-        else:
-            print('Unsupported argument\n')
-    return False
+class Todo(object):
 
-def cl_input():
-    if arguments_validator():
+    def __init__(self):
+        self.my_model = model.Model()
+
+    def arguments_validator(self):
         if len(sys.argv) == 1:
-            model.print_usage()
-        else:
+            return True
+        elif len(sys.argv) == 2:
             if sys.argv[1] == '-l':
-                model.print_todo_list()
+                return True
             elif sys.argv[1] == '-a':
-                model.add_todo(sys.argv[2])
+                print('Unable to add: no task provided')
             elif sys.argv[1] == '-c':
-                model.check_todo(sys.argv[2])            
+                print('Unable to check: no index provided')
             elif sys.argv[1] == '-r':
-                model.remove_todo(sys.argv[2])
-    else:
-        model.print_usage()
+                print('Unable to remove: no index provided')                    
+            else:
+                print('Unsupported argument\n')
+        elif len(sys.argv) == 3:
+            if sys.argv[1] == '-a':
+                return True
+            elif sys.argv[1] == '-c' or sys.argv[1] == '-r':
+                if sys.argv[2].isdigit():
+                    return True
+                else:
+                    print('Unable to remove: index is not a number')
+            else:
+                print('Unsupported argument\n')
+        return False
+
+    def command_line_input(self):
+        if self.arguments_validator():
+            if len(sys.argv) == 1:
+                self.my_model.print_usage()
+            else:
+                if sys.argv[1] == '-l':
+                    self.my_model.print_todo_list()
+                elif sys.argv[1] == '-a':
+                    self.my_model.add_todo(sys.argv[2])
+                elif sys.argv[1] == '-c':
+                    self.my_model.check_todo(sys.argv[2])            
+                elif sys.argv[1] == '-r':
+                    self.my_model.remove_todo(sys.argv[2])
+        else:
+            self.my_model.print_usage()
         
-cl_input()
+my_todo = Todo()
+my_todo.command_line_input()
