320aa259bfb5f03d6083598e8bfa020881e91912
toth bandi <tot70and@gmail.com>
2017-12-14 11:57:27 +0100

    todo - list task

--- model.py
+++ model.py
@@ -7,3 +7,15 @@ Command line arguments:
  -a   Adds a new task
  -r   Removes an task
  -c   Completes an task''')
+
+def print_todo_list():
+    file_name = 'todo_list.txt'
+    try:
+        my_file = open(file_name, 'r')
+        lines = my_file.readlines()
+        for i in range(len(lines)):
+            print('{} - {}'.format(i, lines[i]), end='')
+        print()
+        my_file.close()
+    except IOError:
+        print("cannot open", file_name)
--- todo.py
+++ todo.py
@@ -9,6 +9,9 @@ class Todo(object):
     def cl_input(self):
         if len(self.inputs) == 1:
             model.print_usage()
+        else:
+            if self.inputs[1] == '-l':
+                model.print_todo_list()
     
 my_todo = Todo()
 my_todo.cl_input()
